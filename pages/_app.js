1aedf3360a3e43692c9f32738b3f7922c8656646
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-05-14 12:44:54 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+
+        <title>
+          TecCasa Soluções | Automatismos, Domótica e Vigilância
+        </title>
+
+        <meta
+          name="description"
+          content="Instalação, reparação e assistência técnica em automatismos, domótica, smart home e vigilância doméstica."
+        />
+
+        <link
+          rel="icon"
+          href="/Logo.png"
+        />
+
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
