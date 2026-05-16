d4606906ace7decb56327092f0d3acc93630c0fd
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-05-16 16:24:19 +0100

    Update casas-inteligentes-lisboa.js

--- pages/casas-inteligentes-lisboa.js
+++ pages/casas-inteligentes-lisboa.js
@@ -610,7 +610,7 @@ style={{
                                                                             src={item.image}
                                                                             alt={item.name}
                                                                             style={{
-                                                                            height:item.name === "Zigbee" ? "72px" : "52px",
+                                                                            height:item.name === "Zigbee" ? "88px" : "52px",
                                                                             width:"auto",
                                                                             objectFit:"contain"
                                                                             }}
