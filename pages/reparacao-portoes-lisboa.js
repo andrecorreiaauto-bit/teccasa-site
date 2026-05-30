2d7ba6445f48d1b938830926b2d4f0b915048e44
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-05-30 20:32:55 +0100

    Update reparacao-portoes-lisboa.js

--- pages/reparacao-portoes-lisboa.js
+++ pages/reparacao-portoes-lisboa.js
@@ -609,7 +609,7 @@ style={{
                                                       fontWeight:"bold"
                                                     }}
                                                   >
-                                                    Pedir diagnóstico por WhatsApp
+                                                    Pedir assistência
                                                   </a>
                                               
                                                   <a
