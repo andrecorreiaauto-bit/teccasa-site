47e6d94e78bc56e4a84f93a63954e43304b86cd1
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-06-01 23:23:33 +0100

    Update automatismos-motorline-portoes.js

--- pages/automatismos-motorline-portoes.js
+++ pages/automatismos-motorline-portoes.js
@@ -61,7 +61,7 @@ export default function AutomatismosMotorlinePortoes() {
     document.removeEventListener("click", fecharMenu);
   };
 }, [menuOpen]);
- 
+
   return (
                                                   <>
                                                   <Head>
