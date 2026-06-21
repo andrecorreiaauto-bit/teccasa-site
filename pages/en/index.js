7ab554e2f109cb0cd8a50d9d60429ce7e7b51d73
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-06-21 13:45:06 +0100

    Update index.js

--- pages/en/index.js
+++ pages/en/index.js
@@ -96,7 +96,7 @@ export default function Home() {
                                                   TecCasa Soluções"
                                                   />
                                                   
-                                                  <meta name="robots" content="noindex, nofollow" />
+                                                  
                                                   
                                                   <meta name="viewport" content="width=device-width, initial-scale=1" />
                                                   
