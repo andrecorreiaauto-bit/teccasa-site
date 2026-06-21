2e5f3a63eb46fde72734d087ab5711d5e76ee42d
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-06-21 13:21:13 +0100

    Update garage-remote-controls-lisbon.js

--- pages/en/garage-remote-controls-lisbon.js
+++ pages/en/garage-remote-controls-lisbon.js
@@ -118,6 +118,9 @@ export default function GarageRemoteControlsLisbon() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="canonical" href="https://tec-casa.pt/en/garage-remote-controls-lisbon" />
+        <link rel="alternate" hrefLang="pt-PT" href="https://tec-casa.pt/comandos-garagem" />
+        <link rel="alternate" hrefLang="en" href="https://tec-casa.pt/en/garage-remote-controls-lisbon" />
+        <link rel="alternate" hrefLang="x-default" href="https://tec-casa.pt/comandos-garagem" />
       </Head>
 
       <div className="page">
