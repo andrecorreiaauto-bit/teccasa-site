21c7ac52f79c29fd9ffbf32579da8b4429b3e5d2
andrecorreiaauto-bit <andrecorreia.auto@gmail.com>
2026-06-21 13:20:33 +0100

    Update automatic-gate-installation-lisbon.js

--- pages/en/automatic-gate-installation-lisbon.js
+++ pages/en/automatic-gate-installation-lisbon.js
@@ -119,6 +119,9 @@ export default function AutomaticGateInstallationLisbon() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="canonical" href="https://tec-casa.pt/en/automatic-gate-installation-lisbon" />
+        <link rel="alternate" hrefLang="pt-PT" href="https://tec-casa.pt/instalacao-automatismos-portoes" />
+        <link rel="alternate" hrefLang="en" href="https://tec-casa.pt/en/automatic-gate-installation-lisbon" />
+        <link rel="alternate" hrefLang="x-default" href="https://tec-casa.pt/instalacao-automatismos-portoes" />
       </Head>
 
       <div className="page">
